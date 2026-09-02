9b0de1a88108d76893f72e2721d505a617a8a684
cha0upup <chaodovvn@gmail.com>
2026-09-02 14:03:41 +0800

    refactor: remove global task center and refresh web assets

--- web/src/main/resources/static/assets/useAppEvent-zSo8zlvl.js
+++
@@ -1,1 +0,0 @@
-import{Wt as e}from"./vendor-element-plus-CQCFyCvS.js";var t=new Map;function n(e,n){let r=t.get(e);if(r)for(let e of r)try{e(n)}catch{}}function r(n,r){t.has(n)||t.set(n,new Set),t.get(n).add(r),e(()=>{let e=t.get(n);e&&(e.delete(r),e.size===0&&t.delete(n))})}export{r as n,n as t};
---
+++ web/src/main/resources/static/assets/useSessionParams-BIu0lgh7.js
@@ -0,0 +1,1 @@
+import{pn as e}from"./vendor-element-plus-BCs8NH0s.js";function t(t){function n(){return typeof t==`function`?t():e(t)}function r(e={}){return{sessionId:n(),...e}}return{withSession:r,sessionId:n}}export{t};
